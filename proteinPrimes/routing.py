0b64c6e8e9a11db04ace5536f409b638c38adb3c
Moritz Schlichting <2718281@protonmail.ch>
2018-11-01 00:15:01 +0000

    Removed redundant code

--- proteinPrimes/routing.py
+++ proteinPrimes/routing.py
@@ -6,14 +6,3 @@ channel_routing = [
     route("websocket.connect", consumers.ws_connect),
     route("websocket.receive", consumers.ws_receive),
 ]
-#  from channels.routing import ProtocolTypeRouter, URLRouter
-#  from channels.auth import AuthMiddlewareStack
-#  import primeQure.routing
-#
-#  application = ProtocolTypeRouter({
-    #  'websocket': AuthMiddlewareStack(
-        #  URLRouter(
-            #  primeQure.routing.websocket_urlpatterns
-        #  )
-    #  ),
-#  })
